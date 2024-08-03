9a9e50875b460b36fe9e60791a00e1a1fd8a42b6
hihuss <hihusssss@gmail.com>
2024-08-03 14:02:04 +0300

    add app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,21 @@
-// TODO: write code here
+const place  =  document.querySelector('.goblin');
+const game  =  document.querySelector('.game');
+
+
+
+
+setInterval(() => {
+    for (let i of game.children) {
+        let index=Math.floor(Math.random()*4);
+        i.children[index].append(place);
+        if (i.children[index] === i.lastElementChild){
+            i[0].children[index].append(place);
+        } 
+    }
+      
+     
+}, 1000);
+
+     
+
+
